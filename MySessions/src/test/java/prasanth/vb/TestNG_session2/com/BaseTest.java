971b446430d7e5de39ec475f925a2a99a6d1bd37
Prasanth V B <prasanth.vb@listertechnologies.com>
2021-08-03 13:21:32 +0530

    TestNG Annotations Part 1

--- MySessions/src/test/java/prasanth/vb/TestNG_session2/com/BaseTest.java
+++ MySessions/src/test/java/prasanth/vb/TestNG_session2/com/BaseTest.java
@@ -4,8 +4,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
 public class BaseTest {
@@ -13,11 +13,10 @@ public class BaseTest {
 	public WebDriver driver;
 	
 	@Parameters({ "URL" })
-	@BeforeMethod	
+	@BeforeClass	
 	public void Start(String url) {
 		chrome(url);
 	}
-	
 	
 	private void chrome(String URL) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Prasanth V B\\chromedriver_win32\\chromedriver.exe");
@@ -27,8 +26,8 @@ public class BaseTest {
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
 	
-	@AfterMethod
+	@AfterClass
 	public void quit() {
-		driver.close();
+		driver.quit();
 	}
 }
--- MySessions/src/test/java/prasanth/vb/TestNG_session2/com/Testcase2.java
+++ MySessions/src/test/java/prasanth/vb/TestNG_session2/com/Testcase2.java
@@ -1,10 +1,13 @@
 package prasanth.vb.TestNG_session2.com;
 
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
-public class Testcase1 extends BaseTest{
+public class Testcase2 extends BaseTest{
 
 	@Test
 public void Test1() {
@@ -21,12 +24,21 @@ public void Test2() {
 	searchbox.sendKeys("Printed Chiffon Dress");
 	driver.findElement(By.xpath("//button[@name='submit_search']")).click();
 
-	boolean displayed = driver.findElement(By.xpath("(//a[@class='product_img_link'])[1]")).isDisplayed();
-	if (displayed == true) {
+	WebElement item = driver.findElement(By.xpath("(//a[@class='product_img_link'])[1]"));
+	if (item.isDisplayed() == true) {
+		Actions act = new Actions(driver);
+		act.moveToElement(item).perform();
 		System.out.println("Site search Verified, Hence search functionality is tested successfully");
 	} else {
 		System.out.println("Search Failed");
 	}
 }
 
+@Test(dependsOnMethods = "Test2")
+public void Test3() {
+	driver.findElement(By.xpath("//a[contains(text(),'Printed Chiffon Dress')]/parent::h5/following-sibling::div[@class='button-container']//a[1]")).click();
+	WebElement addcart = driver.findElement(By.xpath("//h2[text()[normalize-space()='Product successfully added to your shopping cart']]"));
+	assertTrue(addcart.isDisplayed()== true, null);
+			
+}
 }
